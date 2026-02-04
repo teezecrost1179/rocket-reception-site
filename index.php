a44b44311766e11518f67443400868e3677fc3eb
teezecrost <hello@rocketsciencedesigns.com>
2026-02-04 15:34:49 -0600

    nix about container

--- index.php
+++ index.php
@@ -2,27 +2,17 @@
 
 <main>
     <!-- HERO -->
-    <section id="about" class="section">
-        <div class="max-width">
-            <section class="hero-section">
-                <div class="hero-content">
-                    <h1 class="hero-headline">Fast Responses Without Fail</h1>
-                    <p class="hero-support">
-                        Would your SMB benefit from having it's own Intelligent virtual receptionist that answers precisely, right away, at any hour?
-                    </p>
-                </div>
-            </section>
+    <div class="max-width">
+        <section class="hero-section">
+            <div class="hero-content">
+                <h1 class="hero-headline">Fast Responses Without Fail</h1>
+                <p class="hero-support">
+                    Would your SMB benefit from having it's own Intelligent virtual receptionist that answers precisely, right away, at any hour?
+                </p>
+            </div>
+        </section>
 
-            <p>
-                Rocket Reception is a practical AI assistant built to help small businesses handle the “same questions, every day” without losing the human touch.
-                It’s designed to connect to the tools you already use—like email (Gmail/Outlook), scheduling (Square Appointments), and voice/chat—so it can
-                find the right context fast, surface what matters, and help you respond confidently.
-            </p>
-            <p>
-                The goal isn’t to replace people. It’s to reduce the back-and-forth, keep customers informed, and free you up for the work that actually needs you.
-            </p>
-        </div>
-    </section>
+    </div>
 
     <?php include 'rocket-contact-form.php'; ?>
 </main>
